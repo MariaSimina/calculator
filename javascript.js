a677080acee261676e53dea46bda5ca371dcb211
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-10 17:09:59 +0300

    Add add, subtract, multiply, and divide

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const operator = "+";
+const firstNumber = 1;
+const secondNumber = 2;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    if(operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
