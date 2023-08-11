9a7f8468682f51d16b37c153eef049b1cbc477e2
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-11 15:12:18 +0300

    Work with first number already written

--- javascript.js
+++ javascript.js
@@ -1,29 +1,47 @@
-const operator = "+";
-const firstNumber = 1;
-const secondNumber = 2;
+const operators = document.querySelectorAll(".operatorButton");
 const display = document.querySelector("#display");
 const numbers = document.querySelectorAll(".numberButton");
+const clear = document.querySelector("#clear");
+
+let tempNumber = 123;
+let displayNumber = "";
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
         display.textContent += number.innerText;
+        displayNumber = display.textContent;
     })
 });
 
+operators.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {
+        let number1 = tempNumber;
+        let number2 = Number(displayNumber);
+        let operator = operatorButton.id;
+        let total = operate(operator, number1, number2);
+        display.textContent = total;
+    })
+});
+
+clear.addEventListener('click', () => {
+    display.textContent = "";
+    displayNumber = "";
+});
+
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(operator, firstNumber, secondNumber) {
